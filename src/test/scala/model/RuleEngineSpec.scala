897cfeec667c9502dc687672f9d9ebabd215fffc
oas <omar.agudo@gmail.com>
2017-02-24 17:05:42 +0100

    First commit. Add RuleEngine containing rules logic

---
+++ src/test/scala/model/RuleEngineSpec.scala
@@ -0,0 +1,13 @@
+import model.RuleEngine
+import org.scalatest.FunSpec
+
+class RuleEngineSpec extends FunSpec {
+
+  describe("A RuleEngine") {
+
+    it("should be instanciated") {
+      val ruleEngine = new RuleEngine
+    }
+
+  }
+}
