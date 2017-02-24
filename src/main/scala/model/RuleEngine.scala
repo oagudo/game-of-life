68631649c64ca1c15fed3b5d1382617c06c579de
oas <omar.agudo@gmail.com>
2017-02-24 17:38:09 +0100

    Second rule implementation

--- src/main/scala/model/RuleEngine.scala
+++ src/main/scala/model/RuleEngine.scala
@@ -1,5 +1,12 @@
 package model
 
 object RuleEngine {
-  def nextState(c: Cell, neighbours: List[Cell]) : Cell = Dead
+  def nextState(c: Cell, neighbours: List[Cell]) : Cell = {
+    val liveNeighbours = neighbours.count(_ == Live)
+
+    c match {
+      case Live => if (liveNeighbours < 2) Dead else Live
+      case _ => Live
+    }
+  }
 }
--- src/test/scala/model/RuleEngineSpec.scala
+++ src/test/scala/model/RuleEngineSpec.scala
@@ -5,12 +5,16 @@ class RuleEngineSpec extends FunSpec {
 
   describe("A RuleEngine") {
 
-    it("should kill any lived cell with no live neighbour") {
+    it("should kill any live cell with no live neighbour") {
       assert(RuleEngine.nextState(Live, List(Dead)) == Dead)
     }
 
-    it("should kill any lived cell with one live neighbour") {
+    it("should kill any live cell with one live neighbour") {
       assert(RuleEngine.nextState(Live, List(Live)) == Dead)
+    }
+
+    it("should keep alive any live cell with two live neighbours") {
+      assert(RuleEngine.nextState(Live, List(Live, Live)) == Live)
     }
 
   }
